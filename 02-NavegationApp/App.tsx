6b6c365d7c0a21487b9fc3c2509100cd72726517
Carlos-Huane <carloshuanesarmiento@gmail.com>
2022-11-13 09:21:52 -0500

    NavegationApp-ReactNative && Typescript

---
+++ 02-NavegationApp/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>
+        App
+      </Text>
+    </View>
+  )
+}
+
+export default App
